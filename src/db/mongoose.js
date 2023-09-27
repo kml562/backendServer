106002015bcd3bc9f411696750197fcebdc05574
kml562 <111355794+kml562@users.noreply.github.com>
2023-09-27 21:55:52 +0530

    done

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,19 @@
-import mongoose from "mongoose";
+// import  mongoose from "mongoose";
 
-export const startServer = async (app,port,url)=>{
-    try {
-        mongoose.set('strictQuery',true);
-        await mongoose.connect(url,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useCreateIndex:true
-        })
-        console.log("Connected to DB");
-        app.listen(port,()=>{
-            console.log(`Running Up the HIll at ${port} km\hr`)
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+// export const startSever =async (app,port, url) => {
+// try {
+//     mongoose.set('strictQuery', true)
+//     await  mongoose.connect(url,{
+//           useNewUrlParser: true,
+//           useUnifiedTopology: true
+//       })
+//       console.log("Connected to DB")
+
+
+//       app.listen(port, ()=>{
+//           console.log(`Running Up The Hill At ${port}km/hr`)
+//       })
+// } catch (error) {
+//     console.log(error);
+// }
+// }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import * as dotenv from 'dotenv';
-import { startServer } from './db/mongoose';
-import app from './app.js';
 dotenv.config();
-
 const {PORT , MONGO_URI}= process.env;
-startServer(app, PORT ,MONGO_URI);
+const startSever =async (app,port,url) => {
+    try {
+        mongoose.set('strictQuery', true)
+        await  mongoose.connect(url,{
+              useNewUrlParser: true,
+              useUnifiedTopology: true
+          })
+          console.log("Connected to DB")
+    
+    
+          app.listen(port, ()=>{
+              console.log(`Running Up The Hill At ${port}km/hr`)
+          })
+    } catch (error) {
+        console.log(error);
+    }
+    }
+startSever(app,PORT,MONGO_URI)
+
+
